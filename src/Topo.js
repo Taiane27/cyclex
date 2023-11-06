36399f97dc93249e4b462a8fd2b33026748e6f64
Taiane Quirino <tayquirino27@gmail.com>
2023-11-06 06:56:20 -0300

    atualização

---
+++ src/Topo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Topo(){
+
+    return (
+        <div>
+            <h2> Loja Site ... </h2>
+        </div>
+    ); 
+}
+
+export default Topo;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Topo from './Topo';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Topo />
     <App />
   </React.StrictMode>
 );
